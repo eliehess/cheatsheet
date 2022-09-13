ff6c19408a9c8438370d57cd3dae709251925ec3
Elie Hess <ehess@athenahealth.com>
2022-09-13 14:24:17 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,67 @@
-use std::collections::VecDeque;
 use std::env::{args, current_exe};
 use std::fs;
 use std::path::{Path, PathBuf};
 use opener::open;
 
+macro_rules! exit {
+    ($($x:expr),*) => ({
+        eprintln!($($x),*);
+        std::process::exit(0);
+    });
+}
+
 fn main() {
-    let args: VecDeque<String> = args().collect();
+    let args: Vec<String> = args().collect();
 
     if args.len() != 2 {
-        println!("Usage: cheatsheet <name>");
-        return;
+        exit!("Usage: cheatsheet <name>");
     }
 
-    let mut dir = current_exe().unwrap();
-    dir.pop();
-    match get_matching_files(&dir, &args[1]) {
-        Ok(files) => {
-            match files.len() {
-                1 => { open(format!("{}", files[0].display())).unwrap(); },
-                0 => { println!("No files found"); },
-                _ => { 
-                    println!("Multiple matching files found, please refine your search or rename them:"); 
-                    for f in files { println!("\t{}", f.display()) } 
-                }
-            }
-        },
-        Err(e) => { println!("{}", e) }
+    let dir = match current_exe() {
+        Err(_) =>  exit!("Unable to get current folder"),
+        Ok(mut exe) => {
+            exe.pop();
+            exe
+        }
     };
+
+    let files = match get_matching_files(&dir, &args[1]) {
+        Err(e) => exit!("{}", e),
+        Ok(files) => files
+    };
+
+    let file = match files.len() {
+        0 => exit!("No matching files found"),
+        1 => &files[0],
+        _ => {
+            let mut err = String::from("Multiple matching files found, please refine your search or rename them:"); 
+            for file in files { err = format!("{}\n\t{}", err, file.display()) }
+            exit!("{}", err);
+        }
+    };
+
+    match open(file) {
+        Err(e) => exit!("{}", e),
+        Ok(()) => println!("Opened {}", file.display())
+    }
 }
 
 fn get_matching_files(dir: &Path, pattern: &str) -> Result<Vec<PathBuf>, String> {
-    Ok(get_files(dir)?
-        .into_iter()
-        .filter(|pathbuf| pathbuf_contains_pattern_ignore_case(pathbuf, pattern))
-        .collect()
-    )
-}
-
-fn get_files(dir: &Path) -> Result<Vec<PathBuf>, String> {
     if !dir.is_dir() {
         return Err(String::from("You must provide a path to a directory, not to a file"));
     }
 
-    match fs::read_dir(dir) {
-        Err(e) => { return Err(e.to_string()) },
-        Ok(dir) => {
-            Ok(dir
-                .filter(|result| result.is_ok())
-                .map(|result| result.unwrap().path())
-                .filter(|pathbuf| pathbuf.is_file())
-                .collect()
-            )
-        }
-    }
+    let read_dir = match fs::read_dir(dir) {
+        Err(e) => return Err(e.to_string()),
+        Ok(read_dir) => read_dir
+    };
+
+    Ok(read_dir
+        .filter(|result| result.is_ok())
+        .map(|result| result.unwrap().path())
+        .filter(|pathbuf| pathbuf.is_file() && pathbuf_contains_pattern_ignore_case(pathbuf, pattern))
+        .collect()
+    )
 }
 
 fn pathbuf_contains_pattern_ignore_case(pathbuf: &PathBuf, pattern: &str) -> bool {
