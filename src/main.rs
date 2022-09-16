74bf6ab4766e76ea6c2f80095575f0a35fefdda6
Elie Hess <ehess@athenahealth.com>
2022-09-16 16:40:45 -0400

    Add markdown processing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env::{args, current_exe};
-use std::fs;
+use std::ffi::OsStr;
+use std::fs::{self, File};
+use std::io::{self, Write};
 use std::path::{Path, PathBuf};
-use opener::open;
+use pulldown_cmark::{Parser, Options, html};
 
 macro_rules! exit {
     ($($x:expr),*) => ({
@@ -25,25 +27,40 @@ fn main() {
         }
     };
 
-    let files = match get_matching_files(&dir, &args[1]) {
+    let files: Vec<PathBuf> = match get_matching_files(&dir, &args[1]) {
         Err(e) => exit!("{}", e),
         Ok(files) => files
     };
 
-    let file = match files.len() {
+    let mut file: PathBuf = match files.len() {
         0 => exit!("No matching files found"),
-        1 => &files[0],
+        1 => files[0].to_path_buf(),
         _ => {
-            let mut err = String::from("Multiple matching files found, please refine your search or rename them:"); 
-            for file in files { err = format!("{}\n\t{}", err, file.display()) }
-            exit!("{}", err);
+            match check_markdown_condition(&files) {
+                Some(file) => file.to_path_buf(),
+                None => exit!("{}", create_multi_file_error(&files))
+            }
         }
     };
 
-    match open(file) {
-        Err(e) => exit!("{}", e),
-        Ok(()) => println!("Opened {}", file.display())
+    if let Some(ext) = file.extension().and_then(OsStr::to_str) {
+        if ext == "md" {
+            file = match convert_markdown_to_html(&file) {
+                Err(e) => exit!("{}", e),
+                Ok(f) => f
+            };
+        }
     }
+
+    let filename = match file.as_path().file_name().and_then(OsStr::to_str) {
+        None => exit!("Unable to get filename"),
+        Some(f) => f
+    };
+
+    match opener::open(&file) {
+        Err(e) => exit!("{}", e),
+        Ok(()) => println!("Opened {}", filename)
+    };
 }
 
 fn get_matching_files(dir: &Path, pattern: &str) -> Result<Vec<PathBuf>, String> {
@@ -65,13 +82,56 @@ fn get_matching_files(dir: &Path, pattern: &str) -> Result<Vec<PathBuf>, String>
 }
 
 fn pathbuf_contains_pattern_ignore_case(pathbuf: &PathBuf, pattern: &str) -> bool {
-    match pathbuf.file_stem() {
+    match pathbuf.file_stem().and_then(OsStr::to_str) {
         None => false,
-        Some(os_str) => {
-            match os_str.to_str() {
-                None => false,
-                Some(string) => string.to_lowercase().contains(&(pattern.to_lowercase()))
+        Some(string) => string.to_lowercase().contains(&(pattern.to_lowercase()))
+    }
+}
+
+fn check_markdown_condition(files: &Vec<PathBuf>) -> Option<&PathBuf> {
+    let mut markdown: Option<&PathBuf> = None;
+    for file in files {
+        match file.extension().and_then(OsStr::to_str) {
+            None => return None,
+            Some(ext) => match ext {
+                "md" => match markdown {
+                    Some(_) => return None,
+                    None => markdown = Some(file)
+                },
+                "html" => (),
+                _ => return None
             }
         }
     }
+    markdown
+}
+
+fn convert_markdown_to_html(markdown: &PathBuf) -> io::Result<PathBuf> {
+    let markdown_input = fs::read_to_string(markdown)?;
+
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_FOOTNOTES);
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    options.insert(Options::ENABLE_TABLES);
+    options.insert(Options::ENABLE_TASKLISTS);
+    let parser = Parser::new_ext(&markdown_input, options);
+
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    let new_file_path = markdown.with_extension("html");
+    let mut output_file = File::create(&new_file_path)?;
+    output_file.write_all(html_output.as_bytes())?;
+
+    Ok(new_file_path)
+}
+
+fn create_multi_file_error(files: &Vec<PathBuf>) -> String {
+    let mut err = String::from("Multiple matching files found, please refine your search or rename them:"); 
+    for file in files {
+        if let Some(f) = file.as_path().file_name().and_then(OsStr::to_str) {
+            err = format!("{}\n\t{}", err, f)
+        }
+    }
+    err
 }
---
+++ src/resources/markdown.md
@@ -0,0 +1,56 @@
+# Markdown
+
+## \#\# Smaller header
+
+### \#\#\# Even smaller header
+
+Regular text
+
+**\*\*Bold text\*\***
+
+*\*Italicized text\**
+
+\~\~~~Struck through text~~\~\~
+
+Unordered List
+- \- one
+- \- two
+- \- seven
+
+Ordered List
+1. 1\. Ay
+2. 2\. Bee
+3. 3\. See
+
+\``code`\`
+
+[\[Link\]\(https://www.example.com\)](https://www.godieinahole.com)
+
+\!\[Image\]\(potato.png\)
+
+![Potato](potato.png)
+
+> \> Block Quote
+>> \>\> Blockier Quote
+
+Checklist
+- [x] \- \[x] aa
+- [ ] \- \[ ] bb
+- [x] \- \[x] cc
+
+footnote \[^1] [^1]
+
+
+Table\||With\||Headers
+:--------|:--------:|--------:
+\:Left align\||\:Center align:\||Right align\:
+
+Horizontal Rule
+
+\-\-\-
+
+---
+
+Footnotes
+
+[^1]: \[^1]: ack ack
